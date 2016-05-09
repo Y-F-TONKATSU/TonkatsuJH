ff2928b637c28d08b1d4ba24431754b7f9c13d8b
Tonkatsu <machbuilders@gmail.com>
2016-05-09 23:05:34 +0900

    Create ShareUtil

---
+++ _dev/assets/script/contents/_tests/share-util-test.js
@@ -0,0 +1,23 @@
+var testShareUtil = function(){
+	
+	mochaSetup();
+	
+	suite('ShareUtil', function() {
+		
+		setup(function(){
+		});
+		
+		test("unko", function() {
+			assert(true, "がんばった");
+		});
+		
+				
+	});
+	
+	$(function(){
+		
+		mocha.run();
+		
+	})
+	
+}();
